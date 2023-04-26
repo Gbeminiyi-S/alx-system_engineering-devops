1d2491e2bd3cf23cc6e5c8fd2bbb4ff03f1fa4f6
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-26 19:36:03 +0000

    Task 1 - update to conform to PEP 8

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+
 """Exports to-do list data for an employee to CSV format"""
+
 import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get("{}users/{}".format(url, sys.argv[1])).json()
-    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
-    data_rows = [[sys.argv[1], user.get("username"), task.get("completed"),
-                 task.get("title")] for task in tasks]
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    tasks = requests.get("{}users/{}/todos".format(url, user_id)).json()
+    data_rows = [
+        [sys.argv[1], user.get("username"), task.get("completed"), task.get("title")]
+        for task in tasks
+    ]
 
     # open a new CSV file in write mode
-    with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as file:
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        [writer.writerow(data) for data in data_rows]
+        for data in data_rows:
+            writer.writerow(data)
