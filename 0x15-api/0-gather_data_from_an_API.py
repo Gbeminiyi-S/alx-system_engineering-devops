d49f5b068bf39a4cf7f104af7f83fc7c9c96b258
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-26 16:08:09 +0000

    update to module documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns information about an employee's TODO list progress"""
+"""Return an employee's TODO list progress."""
 import requests
 import sys
 
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     name = requests.get(f"{url}users/{sys.argv[1]}").json().get("name")
     tasks = requests.get(f"{url}users/{sys.argv[1]}/todos").json()
     c_tasks = [task for task in tasks if task.get("completed") is True]
-    titles = [task.get("title") for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
 
     print(f"Employee {name} is done with tasks({len(c_tasks)}/{len(tasks)}):")
     for title in titles:
