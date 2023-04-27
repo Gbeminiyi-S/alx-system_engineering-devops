002b5051d757b8df7aa0da99e000bab4d13cedf6
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-27 07:47:08 +0000

    latest update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,11 +11,8 @@ if __name__ == "__main__":
     users = requests.get("{}users".format(url)).json()
     tasks = requests.get("{}todos".format(url)).json()
     data_dict = {
-                user.get("id"): [{"username": user.get("username"),
-                                  "task": task.get("title"),
-                                  "completed": task.get("completed")}
-                                 for task in tasks] for user in users
            }
+    print(data_dict)
     # open a new JSON file in write mode
     with open("todo_all_employees.json", "w", newline="") as file:
         json.dump(data_dict, file)
