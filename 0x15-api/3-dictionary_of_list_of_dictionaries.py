f42f152d8b5bc5d3d29c25c6c5c7f71f227a78f2
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-27 08:05:47 +0000

    update to ensure value-UserID match

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,8 +11,13 @@ if __name__ == "__main__":
     users = requests.get("{}users".format(url)).json()
     tasks = requests.get("{}todos".format(url)).json()
     data_dict = {
+                user.get("id"): [{"username": user.get("username"),
+                                  "task": task.get("title"),
+                                  "completed": task.get("completed")}
+                                 for task in tasks
+                                 if task.get("userId") == user.get("id")]
+                                 for user in users
            }
-    print(data_dict)
     # open a new JSON file in write mode
     with open("todo_all_employees.json", "w", newline="") as file:
         json.dump(data_dict, file)
