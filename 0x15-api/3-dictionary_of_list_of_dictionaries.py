7a0c5fdf5a907fee1467eb1f6371f62704e11a79
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-27 07:36:03 +0000

    Task 3 - Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Exports to-do list data for an employee to JSON format"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}users".format(url)).json()
+    tasks = requests.get("{}todos".format(url)).json()
+    data_dict = {
+                user.get("id"): [{"username": user.get("username"),
+                                  "task": task.get("title"),
+                                  "completed": task.get("completed")}
+                                 for task in tasks] for user in users
+           }
+    # open a new JSON file in write mode
+    with open("todo_all_employees.json", "w", newline="") as file:
+        json.dump(data_dict, file)
