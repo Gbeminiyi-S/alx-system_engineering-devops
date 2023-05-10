837737ef9746c2a2ffd1a33b43e7c5067c89f196
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-10 16:55:01 +0000

    Task 2 - update to params

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ def recurse(subreddit, hot_list=[], after=""):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:89.0)\
                Gecko/20100101 Firefox/89.0"}
-    params = {"after": after, "count": count, "limit": 100}
+    params = {"after": after, "limit": 100}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 200:
