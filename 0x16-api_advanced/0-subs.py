e67fcd9009ac66a5e1e5093fb61636efa9e2827d
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-10 16:31:44 +0000

    Task 0 - How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers for a subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:89.0)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:89.0)\
                Gecko/20100101 Firefox/89.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
